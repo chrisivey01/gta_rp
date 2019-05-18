6f6f8db7ecb62f40f8433008a1dea33bded02cca
Chris Ivey <chrisivey01@gmail.com>
2019-05-18 17:14:04 -0500

    update

--- components/total.js
+++ components/total.js
@@ -1,12 +1,12 @@
 module.exports = {
-    weekly: async (message, database) => {
+    total: async (message, database) => {
         const sql = "SELECT * from gta_rp WHERE total_hours IS NOT NULL";
         const results = await database.query(sql);
         try {
             for (let player of results) {
                 message.channel.send({
                     "embed": {
-                        "description":
+                        "description":g
                             "** Name: **" + "<@" + player.discord_uid + ">" +
                             "** Hours: **" + player.total_hours
                     }
